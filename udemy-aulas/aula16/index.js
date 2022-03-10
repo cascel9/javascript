5dddb913519f7739c3313ae435098a14319d5ad6
Celso Silva <99418565+cascel9@users.noreply.github.com>
2022-03-09 23:26:10 -0300

    novos

---
+++ udemy-aulas/aula16/index.js
@@ -0,0 +1,23 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+
+const teste = alunos.slice(0, -2);
+console.log(teste);
+
+
+// alunos.push('Luiza'); adiciona nomes no array ao fim
+// alunos.push('Fábio'); 
+
+//alunos.unshift('Luiza'); adiciona novos nomes no inicio do array
+//alunos.unshift('Fábio');
+
+// console.log(alunos);
+
+// console.log(alunos[2]); selecio nome do indice 2 "João"
+
+// alunos[0] = 'Celso'; Converto no indice 0 LUIZ para Celso
+
+// alunos.pop(); remove o indice do FIM
+
+//const teste = alunos.slice(0, -2); Fatiamento do fim para o começo
+//console.log(teste);
